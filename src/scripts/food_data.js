e6decbb6ec95358ea8ab83523c5af122d43423ab
IshsGit <aishtahad@yahoo.com>
2022-11-14 12:07:14 -0500

    adding reset

--- src/scripts/food_data.js
+++ src/scripts/food_data.js
@@ -1,20 +1,40 @@
 import objects from "./objects.json";
+import userInput from "./user_inputs";
 
 export default class foodData {
   constructor() {
     this.rightContainer = document.getElementById("right-container");
   }
 
-  showDescription() {
+  showDescription(tag) {
     objects.forEach((recipe, idx) => {
+      if(recipe.tags.includes(tag)){
       const descriptionBlock = document.createElement("div");
       descriptionBlock.className = "description-block";
       const text = document.createElement("p");
-      text.className = "description";
-      text.classList.add(recipe.name);
+      text.className = recipe.name;
+      // text.classList.add(recipe.name);
       text.innerText = recipe.description;
       descriptionBlock.appendChild(text);
       this.rightContainer.appendChild(descriptionBlock);
+      }
+    });
+  }
+
+  showCalories(tag){
+    objects.forEach((recipe, idx) => {
+      if(recipe.tags.includes(tag)){
+      const descriptionBlock = document.createElement("div");
+      descriptionBlock.className = "description-block";
+      const text = document.createElement("p");
+      text.className = recipe.name;
+      // text.classList.add(recipe.name);
+      text.innerText = recipe.description;
+      descriptionBlock.appendChild(text);
+      this.rightContainer.appendChild(descriptionBlock);
+      }
     });
   }
 }
+
+
