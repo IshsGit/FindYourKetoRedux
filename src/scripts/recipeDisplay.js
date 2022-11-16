40d732ec4fbec6d771605013b32da618a54d117c
IshsGit <aishtahad@yahoo.com>
2022-11-16 14:42:53 -0500

    populating popup

--- src/scripts/recipeDisplay.js
+++ src/scripts/recipeDisplay.js
@@ -29,20 +29,55 @@ export default class recipeDisplay{
         const innerCard = document.createElement("button");
         innerCard.className = "popup";
         innerCard.innerText = "click for more details..."
+        let name = cardName.innerText;
         cardName.appendChild(innerCard);
-        this.performActionBox(innerCard, cardName);    
+        this.performActionBox(innerCard, cardName, name);    
     }
 
-    performActionBox(button, cardName){
+    performActionBox(button, cardName, name){
         button.addEventListener("click", () => {
             const hasInfo = document.createElement("p");
-            //popup
-            // hasInfo.className = ""
-            hasInfo.innerText = cardName.innerText;
+            recipes.forEach((recipe)=>{
+                
+                if(recipe.name.includes(name)){
+                    const nameText = document.createElement("p");
+                    nameText.innerText = "Recipe Name: "+ name;
+                    this.mContainer.appendChild(nameText);
 
+                    const description = document.createElement("p");
+                    description.innerText = "\n" + "Description: " + recipe.description;
+                    this.mContainer.appendChild(description);
+
+                    const prepTime = document.createElement("p");
+                    prepTime.innerText = "\n" + "Preptime: " + recipe.prepareTime + " minutes";
+                    this.mContainer.appendChild(prepTime);
+
+                    const cookTime = document.createElement("p");
+                    cookTime.innerText = "\n" + "Cooktime: " + recipe.cookTime + " minutes";
+                    this.mContainer.appendChild(cookTime);
+                        
+                    const ingredients = document.createElement("p");
+                    ingredients.innerText += "\n" + "Ingredients: ";
+                    recipe.ingredients.forEach((ingredient)=>{
+                        const ingName = document.createElement("ul");
+                        ingName.innerText = ingredient.name;
+                        ingredients.appendChild(ingName);
+            
+                        this.mContainer.appendChild(ingredients);
+                    });
+
+                    const stepPTag = document.createElement("p");
+                    stepPTag.innerText = "\n" + "Steps: ";
+                    const stepText = document.createElement("p");
+                    stepText.innerText = recipe.steps;
+                    stepPTag.appendChild(stepText);
+                    this.mContainer.appendChild(stepPTag);
+                }
+            })
+
+            // hasInfo.innerText = cardName.innerText;
             const close = document.createElement("button");
             this.modal.style.display = "block";
-
 
             this.mContainer.appendChild(hasInfo);
             this.mContainer.appendChild(close);
