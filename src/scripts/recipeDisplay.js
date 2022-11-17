7a5a408581c592a6db94598e41d094f998a4e6b4
IshsGit <aishtahad@yahoo.com>
2022-11-17 15:21:53 -0500

    remove console

--- src/scripts/recipeDisplay.js
+++ src/scripts/recipeDisplay.js
@@ -15,7 +15,7 @@ export default class recipeDisplay{
 
     generateCard(recipeNameArr){
         let alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-        console.log("in generate");
+       
         let cardName = "";
         recipeNameArr.forEach((name,idx)=>{
             cardName = document.createElement("button");
